37916fb75ccc16a22abe2ce2dbc9d749b5730197
Primograph <primograph@gmail.com>
2019-11-19 17:20:34 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Nathan Kantorov, Period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Kantorov_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Kantorov_Projects.html">Projects.html</a></li></br>
+      <li><a href="Kantorov_Resume.html">Resume.html</a></li></br>
+      <li><a href="Kantorov_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
